38e22883ecd93cc14e665ab96003d1376b3e3917
jamesdoom <brianpalmer39@gmail.com>
2022-11-01 11:37:07 -0400

    Update js and html to include prompt for grid size. use setProperty to adjust columns based on user input and auto-size grid to maintain square.

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function addBox(){
 
-    
-let grid = document.getElementById("container");
-for (let i=0;i<256;i++){
-let cell = document.createElement("div");
-grid.appendChild(cell);
+let size=prompt("How many squares per side?");
+let side=parseInt(size);
+let n=side*side;
+
+function addBox(){
+    let grid = document.getElementById("container");
+    grid.style.setProperty("grid-template-columns", "repeat("+ side +",auto)");
+    for (let i=0;i<n;i++){
+    let cell = document.createElement("div");
+    grid.appendChild(cell);
 }
+mOver();
+}
+
+function mOver(){
+    let boxes=document.querySelectorAll("div");
+    boxes.forEach(boxes=> {
+        boxes.addEventListener("mouseenter", ()=> {
+            boxes.style.backgroundColor="white";
+        });
+    });
+    boxes.forEach(boxes=> {
+        boxes.addEventListener("mouseout", ()=> {
+            boxes.style.backgroundColor="black";
+        });
+    });
+}
+
+function reset(){
+    let boxes=document.querySelectorAll("div");
+    boxes.forEach(boxes=> {
+            boxes.style.backgroundColor="red";
+    });
 }
